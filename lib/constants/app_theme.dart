94f36cb967853b48b7a2751e5eb9f101912faa81
aditya-10k <katheaditya10@gmail.com>
2025-01-22 14:11:59 +0530

    initail firebase setup , text setup

---
+++ lib/constants/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+// class AppTheme {
+//   static final lightThemeMode = ThemeData.light().copyWith(
+//       //scaffoldBackgroundColor: AppColors.backgroundColor,
+//       bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+//         backgroundColor: AppColors.shadeColor,
+//       ),
+//       colorScheme: ColorScheme.fromSeed(
+//           seedColor: AppColors.cnms_blue, primary: AppColors.cnms_blue));
+// }
---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+
+  // aditya - i will add colors when they are finalized
+}
